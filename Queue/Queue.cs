33e08f0f5385cb52fc3c349c6effce5a34aa3198
Kndzhdn <77060553+Kndzhdn@users.noreply.github.com>
2021-03-03 12:10:07 +0300

    Update Queue.cs

--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -1,63 +1,74 @@
 public class Queue<T>
+{
+    private QueueItem<T> _head;
+    private QueueItem<T> _tail;
+
+    public int Count { get; private set; }
+
+    public bool IsEmpty => Count == 0;
+
+    public Queue()
     {
-        private QueueItem<T> _head;
-        private QueueItem<T> _tail;
+    }
 
-        public int Count { get; private set; }
+    public Queue(T data)
+    {
+        SetHeadItem(data);
+    }
 
-        public bool IsEmpty => Count == 0;
-        
-        public Queue(){}
+    private void SetHeadItem(T data)
+    {
+        var item = new QueueItem<T>(data);
+        _head = item;
+        _tail = item;
+        Count = 1;
+    }
 
-        public Queue(T data)
+    public void Enqueue(T data)
+    {
+        if (IsEmpty)
         {
             SetHeadItem(data);
+            return;
         }
 
-        private void SetHeadItem(T data)
+        var item = new QueueItem<T>(data)
         {
-            var item = new QueueItem<T>(data);
-            _head = item;
-            _tail = item;
-            Count = 1;
-        }
-
-        public void Enqueue(T data)
-        {
-            if (!IsEmpty)
-            {
-                SetHeadItem(data);
-                return;
-            }
-
-            var item = new QueueItem<T>(data)
-            {
-                Next = _tail
-            };
-            _tail = item;
-            Count++;
-        }
-
-        public T Dequeue()
-        {
-            var data = _head.Data;
-
-            var current = _tail.Next;
-            var previous = _tail;
-            while (current != null && current.Next != null)
-            {
-                previous = current;
-                current = current.Next;
-            }
-
-            _head = previous;
-            _head.Next = null;
-            Count--;
-            return data;
-        }
-
-        public T Top()
-        {
-            return _head.Data;
-        }
+            Next = _tail
+        };
+        _tail = item;
+        Count++;
     }
+
+    public T Dequeue()
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("Очередь пуста.");
+        }
+
+        var data = _head.Data;
+
+        var current = _tail.Next;
+        var previous = _tail;
+        while (current != null && current.Next != null)
+        {
+            previous = current;
+            current = current.Next;
+        }
+
+        _head = previous;
+        _head.Next = null;
+        Count--;
+        return data;
+    }
+
+    public T Top()
+    {
+        if (IsEmpty)
+        {
+            throw new InvalidOperationException("Очередь пуста.");
+        }
+        return _head.Data;
+    }
+}
